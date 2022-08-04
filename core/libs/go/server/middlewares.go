5ed51a9a2160a74f2e4524464d42213e7afc16c7
Santiago Ruta <ruta2366@gmail.com>
2022-08-04 11:27:43 -0300

    Removes HeaderVerification debug log

--- core/libs/go/server/middlewares.go
+++ core/libs/go/server/middlewares.go
@@ -64,7 +64,7 @@ func HeaderVerification(header string, value string) gin.HandlerFunc {
 			errors.ReturnError(c, &errors.Error{
 				Code:    errors.ForbiddenApiError,
 				Cause:   "comparing header value",
-				Message: fmt.Sprintf("invalid %s token, required: %s", header, value),
+				Message: fmt.Sprintf("invalid %s token", header),
 			})
 			c.Abort()
 			return

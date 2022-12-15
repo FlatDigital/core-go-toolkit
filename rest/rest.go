731b54171edafea3c5f23e6eea4db0dcfd1d7bac
Nicolás de Lara <nicodelara@gmail.com>
2022-12-15 11:11:11 -0300

    return headers in an http response && fix mocks

--- rest/rest.go
+++ rest/rest.go
@@ -10,14 +10,14 @@ import (
 
 // Rest interface for rest service
 type Rest interface {
-	MakeGetRequest(ctx *flat.Context, url string, headers http.Header) (int, []byte, error)
-	MakePostRequest(ctx *flat.Context, url string, body interface{}, headers http.Header) (int, []byte, error)
-	MakePutRequest(ctx *flat.Context, url string, body interface{}, headers http.Header) (int, []byte, error)
-	MakePatchRequest(ctx *flat.Context, url string, body interface{}, headers http.Header) (int, []byte, error)
-	MakeDeleteRequest(ctx *flat.Context, url string, headers http.Header) (int, []byte, error)
+	MakeGetRequest(ctx *flat.Context, url string, headers http.Header) (int, []byte, http.Header, error)
+	MakePostRequest(ctx *flat.Context, url string, body interface{}, headers http.Header) (int, []byte, http.Header, error)
+	MakePutRequest(ctx *flat.Context, url string, body interface{}, headers http.Header) (int, []byte, http.Header, error)
+	MakePatchRequest(ctx *flat.Context, url string, body interface{}, headers http.Header) (int, []byte, http.Header, error)
+	MakeDeleteRequest(ctx *flat.Context, url string, headers http.Header) (int, []byte, http.Header, error)
 
 	MakeGetRequestWithConfig(ctx *flat.Context, url string, headers http.Header,
-		config RequestConfig) (int, []byte, error)
+		config RequestConfig) (int, []byte, http.Header, error)
 	MakePostRequestWithConfig(ctx *flat.Context, url string, body interface{}, headers http.Header,
 		config RequestConfig) (int, []byte, error)
 	MakePutRequestWithConfig(ctx *flat.Context, url string, body interface{}, headers http.Header,
